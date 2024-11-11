24e4edfc3cf3aa41b25de0af1fdeef7b5da2a270
Shivam Chopra <parthasus7973@gmail.com>
2024-11-11 18:54:45 +0530

    Setup Slices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,18 +7,17 @@ import { createClient } from "@/prismicio";
 import Header from "@/componenets/header";
 import Footer from "@/componenets/footer";
 
-
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
   const page = await client.getSingle("settings");
- 
+
   return {
     title: page.data.site_title || "fallback title",
     description: page.data.meta_description || "fallback description",
     openGraph: {
       images: [page.data.og_image.url || "image"],
     },
-  }
+  };
 }
 export default function RootLayout({
   children,
@@ -27,10 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
-      <body
-        className={clsx(nunito.variable, nunito_sans.variable)}
-      >
+      <body className={clsx(nunito.variable, nunito_sans.variable)}>
         <Header></Header>
         {children}
         <Footer></Footer>
